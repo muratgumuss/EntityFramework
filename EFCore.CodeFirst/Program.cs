af01f9d3d41cde616d6fb4f0623d24e9d8344c9c
Murat Gümüş <muratgumus05@gmail.com>
2026-05-11 19:25:38 +0300

    db context states

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -1,6 +1,36 @@
 ﻿using EFCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
 
 using (var context = new AppDbContext())
 {
+    var newProduct = new Product
+    {
+        Name = "Iphone 15 Pro Max",
+        Price = 30000,
+        DiscountPrice = 25000,
+        Stock = 100,
+        Barcode = 123456789,
+        CategoryId = 1
+    };
+
+    var product = await context.Products.FirstOrDefaultAsync(p => p.Id == 10);
+
+    Console.WriteLine($"Before Adding - State: {context.Entry(newProduct).State}");
+    //await context.AddAsync(newProduct);
+    // context.Entry(newProduct).State = EntityState.Added;
+    context.Remove(product);
+    await context.SaveChangesAsync();
+    Console.WriteLine($"After Adding - State: {context.Entry(newProduct).State}");
+    
+
+    Console.WriteLine($"After Save - State: {context.Entry(newProduct).State}");
+
+    var products = await context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        var state = context.Entry(p).State;
+        Console.WriteLine($"Id: {p.Id} - Name: {p.Name} - Price: {p.Price} - State: {state}");
+    });
 
 }
