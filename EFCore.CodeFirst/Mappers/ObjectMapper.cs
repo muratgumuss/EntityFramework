75d69df0f5a8c277f564de73932a03a90ca84f1a
Murat Gümüş <muratgumus05@gmail.com>
2026-06-05 20:20:12 +0300

    projections

---
+++ EFCore.CodeFirst/Mappers/ObjectMapper.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using EFCore.CodeFirst.DAL;
+using EFCore.CodeFirst.Dtos;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace EFCore.CodeFirst.Mappers
+{
+    public class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> layz = new Lazy<IMapper>(() => {
+            var config = new MapperConfiguration(cfg => { cfg.AddProfile<CustomMapping>(); }, NullLoggerFactory.Instance);
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => layz.Value;
+    }
+
+    internal class CustomMapping : Profile
+    {
+
+        public CustomMapping()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+        }
+    }
+
+
+}
