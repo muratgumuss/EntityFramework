884371c3c411b7f531737090f3e2f30771b4e032
Murat Gümüş <muratgumus05@gmail.com>
2026-06-16 20:02:19 +0300

    concurrency

---
+++ Concurrency.Web/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Concurrency.Web.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        // [Timestamp] 1.yol
+        public byte[] RowVersion { get; set; }
+    }
+}
